faaf18955f3f7d58ef9edb21f9a8564efbdf604e
RiicK3d <40563427+RiicK3d@users.noreply.github.com>
2023-01-20 10:57:25 +0000

    added pcl cloud and basic segmentation

--- agi_vision/scripts/get_image.py
+++ agi_vision/scripts/get_image.py
@@ -36,14 +36,78 @@
 ## Simple talker demo that listens to std_msgs/Strings published 
 ## to the 'chatter' topic
 
+# Import modules
 import rospy
+# import ros_numpy
+import pcl
+# https://github.com/strawlab/python-pcl
+import numpy as np
+import ctypes
+import struct
+import sensor_msgs.point_cloud2 as pc2
+
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
+from random import randint
 from std_msgs.msg import String
 from sensor_msgs.msg import Image 
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + ('image  width %s and height %s' % (data.width, data.height)))
+
+# from  https://github.com/jupidity/PCL-ROS-cluster-Segmentation/blob/master/scripts/pcl_helper.py
+def ros_to_pcl(ros_cloud):
+    """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
     
-def image_getter():
+        Args:
+            ros_cloud (PointCloud2): ROS PointCloud2 message
+            
+        Returns:
+            pcl.PointCloud_PointXYZRGB: PCL XYZRGB point cloud
+    """
+    points_list = []
+
+    for data in pc2.read_points(ros_cloud, skip_nans=True):
+        points_list.append([data[0], data[1], data[2], data[3]])
+
+    pcl_data = pcl.PointCloud_PointXYZRGB()
+    pcl_data.from_list(points_list)
+
+    return pcl_data
+
+def segment_pcl(cloud):
+    fil = cloud.make_passthrough_filter()
+    fil.set_filter_field_name("z")
+    fil.set_filter_limits(0, 1.5)
+    cloud_filtered = fil.filter()
+
+    # print(cloud_filtered.size)
+
+    seg = cloud_filtered.make_segmenter_normals(ksearch=50)
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+    seg.set_normal_distance_weight(0.1)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_max_iterations(100)
+    seg.set_distance_threshold(0.03)
+    indices, model = seg.segment()
+    print(len(indices))
+    print(model)
+
+def callback(data):
+    # gets pointcloud2 data from node 
+    if not data.is_dense:
+        rospy.logwarn('invalid points in Pointcloud!')
+    # rospy.loginfo(rospy.get_caller_id() + ('image  width %s and height %s' % (data.width, data.height)))
+
+    
+    # for p in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True):
+    #     print " x : %f  y: %f  z: %f" %(p[0],p[1],p[2])
+    pcl_data = ros_to_pcl(data)
+    #print(pcl)
+    segment_pcl(pcl_data)
+
+# link for clusters: https://github.com/jupidity/PCL-ROS-cluster-Segmentation/blob/master/src/segmentation.cpp
+    
+def pose_estimation():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
@@ -52,10 +116,14 @@ def image_getter():
     # run simultaneously.
     rospy.init_node('image_getter', anonymous=True)
 
-    rospy.Subscriber('xtion/rgb/image_color', Image, callback)
+    # http://docs.ros.org/en/api/sensor_msgs/html/msg/PointCloud2.html
+    rospy.Subscriber('/xtion/depth_registered/points', PointCloud2, callback)
+    # /xtion/depth_registered/camera_info
+    # /xtion/depth_registered/image_raw
+    # /xtion/depth_registered/points
     rospy.loginfo('started get image node') # %s and height %s' % (data.width, data.height))
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    image_getter()
+    pose_estimation()
