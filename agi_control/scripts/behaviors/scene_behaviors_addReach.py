704d018a3c1c658512ac732c7c7525010f3abd30
Qiao <ffff0f0f@163.com>
2023-01-30 18:33:04 +0100

    BTs add reach

---
+++ agi_control/scripts/behaviors/scene_behaviors_addReach.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This behavior is used to get the scene blocks from the scene and store them in the blackboard."""
+
+import py_trees
+import rospy
+from geometry_msgs.msg import PoseStamped
+import py_trees.console as console
+from py_trees.blackboard import Blackboard
+from moveit_msgs.srv import GetPlanningScene
+import sys
+sys.path.append('..')
+from reachability import Arm
+
+
+class GetSceneBlocks(py_trees.behaviour.Behaviour):
+    """
+    GetSceneBlocks is a behavior that retrieves the collision objects of the planning scene, filters the objects
+    to select only the ones that have an id that starts with 'box', and stores them in the blackboard.
+    """
+
+    def __init__(self, name="GetSceneBlocks"):
+        """
+        Initialize the behavior.
+
+        Args:
+            name (str): The name of the behavior
+        """
+        super(GetSceneBlocks, self).__init__(name=name)
+        self.blackboard = Blackboard()
+
+        self.scene_srv = rospy.ServiceProxy("/get_planning_scene",
+                                            GetPlanningScene)
+        self.scene_srv.wait_for_service()
+
+    def update(self):
+        """
+        Retrieve the collision objects from the planning scene, filter them to select only the ones that have an id that starts with 'box',
+        and store them in the blackboard.
+
+        Returns:
+            py_trees.common.Status.SUCCESS
+        """
+        scene = self.scene_srv().scene.world.collision_objects
+        blocks = [obj for obj in scene if obj.id.startswith("box")]
+        stack_blocks = self.blackboard.blocks_in_stack
+        # Remove the blocks that are already in the stack
+        if len(stack_blocks) > 0:
+            blocks = [
+                block for block in blocks if block.id not in
+                [stack_block.id for stack_block in stack_blocks]
+            ]
+
+        self.blackboard.scene_blocks = blocks
+
+        return py_trees.common.Status.SUCCESS
+
+
+class PopNextBlock(py_trees.behaviour.Behaviour):
+
+    def __init__(self, name="PopNextBlock"):
+        """Initialize the behavior."""
+        super(PopNextBlock, self).__init__(name=name)
+        self.blackboard = Blackboard()
+
+    def update(self):
+        """Pop the next block from the scene blocks list and store it in the blackboard."""
+        if self.blackboard.scene_blocks:
+            self.blackboard.next_block = self.blackboard.scene_blocks.pop()
+            console.loginfo("Poped block: {}".format(
+                self.blackboard.next_block.id))
+
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.FAILURE
+
+
+class GetNextStackLocation(py_trees.behaviour.Behaviour):
+
+    def __init__(self, name="GetNextStackLocation"):
+        """Initialize the behavior."""
+        super(GetNextStackLocation, self).__init__(name=name)
+        self.blackboard = Blackboard()
+        self.blackboard.blocks_in_stack = []
+
+    def initialise(self):
+        """Initialize the behavior."""
+        self.blackboard.next_stack_location = None
+
+    def update(self):
+        """Get the next stack location from the scene and store it in the blackboard."""
+        loc = PoseStamped()
+        loc.header.frame_id = "base_footprint"
+        loc.pose.position.x = 0.6
+        loc.pose.position.y = 0.75 / 2
+        loc.pose.position.z = 0.45 + 0.045 * len(
+            self.blackboard.blocks_in_stack) + 0.01
+        loc.pose.orientation.w = 1.0
+        self.blackboard.next_stack_location = loc
+        
+        # selsect arm
+        arm = Arm(loc.pose.position.x,loc.pose.position.y,loc.pose.position.z)        
+        self.left_right = arm.getArm()
+        
+        return py_trees.common.Status.SUCCESS
+
+
+class ResetNextBlock(py_trees.behaviour.Behaviour):
+
+    def __init__(self, name="ResetNextBlock"):
+        """Initialize the behavior."""
+        super(ResetNextBlock, self).__init__(name=name)
+        self.blackboard = Blackboard()
+
+    def update(self):
+        """Reset the next block in the blackboard."""
+        self.blackboard.next_block = None
+        return py_trees.common.Status.SUCCESS
--- agi_control/scripts/reachability.py
+++ agi_control/scripts/reachability.py
@@ -3,101 +3,82 @@
 '''
 According to the subscribed '/cube_pose' topic, 
 judge the reachability of the left and right hands, 
-and then publish the '/armNum' topic (0=unreachable, 1=left, 2=right)
 
 Note: map files in the map folder need to change permissions (chmod a+x file)
 '''
 
 import os
 import numpy as np
-import rospy
-from geometry_msgs.msg import Pose, PoseStamped
-from std_msgs.msg import Int8
+import random
+import sys
+
+sys.path.append('..')
+# from maps import f
 
 
+class Arm():
+        
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.pos = np.array(([self.x,self.y,self.z]))
+        # print(self.pos)
+
+    def reachMap(self):
+        # npy file -> array (reach map)
+        path_curr = os.getcwd()
+        path_pre = os.path.abspath(os.path.dirname(os.getcwd()))
+        if os.path.exists(os.path.join(path_curr,'maps')):
+            path_maps = path_curr
+        else:
+            path_maps = path_pre
+        path_map_left = os.path.join(os.path.dirname(path_maps), \
+                                    'maps/filtered_3D_reach_map_gripper_left_grasping_frame_torso_False_0.05_2022-08-29-19-04-04.npy')
+        path_map_right = os.path.join(os.path.dirname(path_maps), \
+                                    'maps/filtered_3D_reach_map_gripper_right_grasping_frame_torso_False_0.05_2022-08-29-19-04-04.npy')
+        map_left = np.load(path_map_left)    #(x,y,z,score)
+        map_right = np.load(path_map_right)
+        return map_left, map_right
+
+    def score(self,map,pos):
+        # reach map,position -> score
+        dist = np.empty((map.shape[0],1))
+        for i,l in enumerate(map):
+            dist[i] = np.sqrt(np.sum(np.square(l[:3]-pos))) 
+        # which cell of 5cm boxel grid
+        index = np.argmin(dist)
+        score = map[index,3]
+        return score
+
+    def selectArm(self,score_l, score_r): 
+        if score_l==0 and score_r==0:
+            # print("the inputed pose isn't in the reach map")
+            return ""  
+        elif score_l > score_r:
+            # print("left arm")
+            return "left"
+        else:
+            # print("right arm")
+            return "right"
+        
+    def getArm(self):
+        map_left, map_right = self.reachMap()    
+        # better reachability score(0~100) -> select arm
+        score_l = self.score(map_left, self.pos)  # minmal distance and index
+        score_r = self.score(map_right, self.pos)
+        arm = self.selectArm(score_l, score_r)  # unreachable="", left="left", right="right"
+        # print("score_l:",score_l)
+        # print("score_r",score_r)
+        # print(arm)
+        return arm
 
 
-# def init():
-#     x = -1 + 2*random.random()
-#     y = -1 + 2*random.random()
-#     z = -1 + 2*random.random()
-#     pos = np.array(([x,y,z]))
-#     # print(pos)
-#     return pos
-
-def reachMap():
-    # npy file -> array (reach map)
-    path_curr = os.getcwd()
-    path_map_left = os.path.join(os.path.dirname(path_curr), \
-                                'maps/filtered_3D_reach_map_gripper_left_grasping_frame_torso_False_0.05_2022-08-29-19-04-04.npy')
-    path_map_right = os.path.join(os.path.dirname(path_curr), \
-                                'maps/filtered_3D_reach_map_gripper_right_grasping_frame_torso_False_0.05_2022-08-29-19-04-04.npy')
-    map_left = np.load(path_map_left)    #(x,y,z,score)
-    map_right = np.load(path_map_right)
-    return map_left, map_right
-
-def score(map,pos):
-    # reach map,position -> score
-    dist = np.empty((map.shape[0],1))
-    for i,l in enumerate(map):
-        dist[i] = np.sqrt(np.sum(np.square(l[:3]-pos))) 
-    # which cell of 5cm boxel grid
-    index = np.argmin(dist)
-    score = map[index,3]
-    return score
-
-def selectArm(score_l, score_r): 
-    if score_l==0 and score_r==0:
-        # print("the inputed pose isn't in the reach map")
-        return 0   
-    elif score_l > score_r:
-        # print("left arm")
-        return 1
-    else:
-        # print("right arm")
-        return 2
-
-def callback(msg):
-    pos = np.empty((3,1))
-    pos[0] = msg.pose.position.x
-    pos[1] = msg.pose.position.y
-    pos[2] = msg.pose.position.z
-    # rospy.loginfo(pos)
-    
-    map_left, map_right = reachMap()    
-    # better reachability score(0~100) -> select arm
-    score_l = score(map_left, pos)  # minmal distance and index
-    score_r = score(map_right, pos)
-    arm = selectArm(score_l,score_r)  # 0=unreachable, 1=left, 2=right
-    rospy.loginfo("subscribe pose:")
-    rospy.loginfo(msg.pose)
-    try:
-        armNum_publisher(arm)
-    except rospy.ROSInterruptException:
-        pass
-
-
-def pose_subscriber():
-    rospy.init_node('pose_subscriber','armNum_publisher', anonymous=True)
-    rospy.Subscriber("/cube_pose", PoseStamped, callback)
-    rospy.spin()
-    
-def armNum_publisher(arm):
-    # 0=unreachable, 1=left, 2=right
-    # rospy.init_node('armNum_publisher', anonymous=True)
-    armNum_pub = rospy.Publisher('/armNum', Int8, queue_size=1)
-    rate = rospy.Rate(10)
-    # while not rospy.is_shutdown():
-    arm_msg = Int8()
-    arm_msg = arm
-    armNum_pub.publish(arm_msg)
-    rospy.loginfo("publish arm message: %d", arm_msg)
-    rospy.loginfo("--------------------------------")
-    rate.sleep()
      
-def main():
-    pose_subscriber()        
-    
     
 if __name__ == '__main__':
-    main()
+    x = -1 + 2*random.random()
+    y = -1 + 2*np.random.random()
+    z = -1 + 2*random.random()
+    arm = Arm(x,y,z)
+    print(arm.getArm()) 
