ccebc343a927772185a43727d34ca85bf2dd19af
oxFFFF-Q <ffff0f0f@163.com>
2023-02-16 08:52:53 +0100

    update map

--- agi_control/maps/map_process.py
+++ agi_control/maps/map_process.py
@@ -17,55 +17,75 @@ def pkl_p3TOp2():
     path_maps_6D_p2_r = "/home/qiao/Proj_Robman/maps/maps_6D_p2/filt_reach_map_gripper_right_grasping_frame_torso_False.pkl"
     pickle.dump(map_l, open(path_maps_6D_p2_l, "wb"), protocol=2)
     pickle.dump(map_r, open(path_maps_6D_p2_r, "wb"), protocol=2)
-    
-def clip_map_inZ(map, height, boundary):
-    # map, height of the table, boundary -> clipped map along z axis
-    z = map[:,2]
-    inds_z1 = np.argwhere(z<=(height+boundary)).reshape(-1)
-    map_cl_axis_ = map[inds_z1,:]
-    inds_z2 = np.argwhere(map_cl_axis_[:,2]>=(height-boundary)).reshape(-1)
-    map_cl_axis = map_cl_axis_[inds_z2,:]
-    
-    # print(inds_z1.shape)
-    # print(inds_z2.shape)
-    # print("---------------------")
-    
-    # print(np.max(map[:,2]))
-    # print(np.min(map[:,2]))
-    # print(np.max(map_cl_axis_[:,2]))
-    # print(np.min(map_cl_axis_[:,2]))
-    # print("---------------------")
-    print(np.max(map_cl_axis[:,2]))
-    print(np.min(map_cl_axis[:,2]))
-    print("---------------------")
-    
-    
-    # print(map.shape)
-    # print(map_cl_axis.shape)
-    return map_cl_axis
 
-def clip_maps(height, boundary):
-    
+
+def clip_map_axes(map, xyz, boundary):
+
+    # clipped map along x axis
+    x = map[:, 0]
+    inds_x1 = np.argwhere(x < (xyz[0] + boundary[0])).reshape(-1)
+    map_cl_x1 = map[inds_x1, :]
+    inds_x2 = np.argwhere(map_cl_x1[:, 0] > (xyz[0] - boundary[0])).reshape(-1)
+    map_cl_x2 = map[inds_x1[inds_x2], :]
+
+    # clipped map along y axis
+    y = map_cl_x2[:, 1]
+    inds_y1 = np.argwhere(y < (xyz[1] + boundary[1])).reshape(-1)
+    map_cl_y1 = map_cl_x2[inds_y1, :]
+    inds_y2 = np.argwhere(map_cl_y1[:, 1] > (xyz[1] - boundary[1])).reshape(-1)
+    map_cl_y2 = map_cl_x2[inds_y1[inds_y2], :]
+
+    # clipped map along z axis
+    z = map_cl_y2[:, 2]
+    inds_z1 = np.argwhere(z < (xyz[2] + boundary[2])).reshape(-1)
+    map_cl_z1 = map_cl_y2[inds_z1, :]
+    inds_z2 = np.argwhere(map_cl_z1[:, 2] > (xyz[2] - boundary[2])).reshape(-1)
+    map_cl_z2 = map_cl_y2[inds_z1[inds_z2], :]
+
+    print("map shape:", map.shape)
+
+    print("x min:", np.min(map[:,0]), "->", np.min(map_cl_x2[:, 0]))
+    print("x max:", np.max(map[:,0]), "->", np.max(map_cl_x2[:, 0]))
+    print(map_cl_x2.shape)
+
+    print("y min:", np.min(map[:, 1]), "->", np.min(map_cl_y2[:, 1]))
+    print("y max:", np.max(map[:, 1]), "->", np.max(map_cl_y2[:, 1]))
+    print(map_cl_y2.shape)
+
+    print("z min:", np.min(map[:,2]), "->", np.min(map_cl_z2[:, 2]))
+    print("z max:", np.max(map[:,2]), "->", np.max(map_cl_z2[:, 2]))
+    print(map_cl_z2.shape)
+    print("---------------------")
+
+    return map_cl_z2
+
+
+def clip_maps(xyz, boundary):
+
     path_maps_6D_p2_l = "/home/qiao/Proj_Robman/maps/maps_6D_p2/filt_reach_map_gripper_left_grasping_frame_torso_False.pkl"
     path_maps_6D_p2_r = "/home/qiao/Proj_Robman/maps/maps_6D_p2/filt_reach_map_gripper_right_grasping_frame_torso_False.pkl"
     map_l = pickle.load(open(path_maps_6D_p2_l, 'rb'))
     map_r = pickle.load(open(path_maps_6D_p2_r, 'rb'))
-    
-    map_clip_l = clip_map_inZ(map_l, height, boundary)
-    map_clip_r = clip_map_inZ(map_l, height, boundary)
+
+    map_clip_l = clip_map_axes(map_l, xyz, boundary)
+    map_clip_r = clip_map_axes(map_r, xyz, boundary)
 
     map_l_cliped = map_clip_l
     map_r_cliped = map_clip_r
     path_maps_6D_p2_aboveTable_l = "/home/qiao/Proj_Robman/maps/maps_6D_p2_clipped/reach_map_6D_left_clipped.pkl"
     path_maps_6D_p2_aboveTable_r = "/home/qiao/Proj_Robman/maps/maps_6D_p2_clipped/reach_map_6D_right_clipped.pkl"
-    pickle.dump(map_l_cliped, open(path_maps_6D_p2_aboveTable_l , "wb"), protocol=2)
-    pickle.dump(map_r_cliped, open(path_maps_6D_p2_aboveTable_r, "wb"), protocol=2)
+    pickle.dump(map_l_cliped,
+                open(path_maps_6D_p2_aboveTable_l, "wb"),
+                protocol=2)
+    pickle.dump(map_r_cliped,
+                open(path_maps_6D_p2_aboveTable_r, "wb"),
+                protocol=2)
 
 
 if __name__ == '__main__':
-    # pkl_p3TOp2()
+    # pkl_p3TOp2()S
     cube = 0.045
-    height = 0.45 + cube/2   # height of the table
-    boundary = 0.095  # redundant boundary for z axis
-    
-    clip_maps(height,boundary)
+    xyz = [0, 0, 0.45+0.03]  
+    boundary = [(0.75+0.03)/2 + 0.1, (0.75+0.03)/2 + 0.1, 0.2]  # redundant boundary for z axis
+
+    clip_maps(xyz, boundary)
--- agi_control/scripts/reachability_6D.py
+++ agi_control/scripts/reachability_6D.py
@@ -14,109 +14,118 @@ import pickle
 import timeit
 
 
-
-
 class Arm():
-        
-    def __init__(self,pose):
-        self.pos =pose
 
+    def __init__(self, pose):
+        self.pos = pose
 
     def reachMap(self):
-        # npy file -> array (reach map)        
-        start=timeit.default_timer()
-        
+        # npy file -> array (reach map)
+        start = timeit.default_timer()
+
         # search the path of maps folder
         path_curr = os.getcwd()
         path_pre = os.path.abspath(os.path.dirname(path_curr))
-        path_pre_pre = os.path.abspath(os.path.dirname(path_pre))        
-        if os.path.exists(os.path.join(path_curr,'maps')):
+        path_pre_pre = os.path.abspath(os.path.dirname(path_pre))
+        if os.path.exists(os.path.join(path_curr, 'maps')):
             path_maps = path_curr
-        elif os.path.exists(os.path.join(path_pre,'maps')):
+        elif os.path.exists(os.path.join(path_pre, 'maps')):
             path_maps = path_pre
         else:
             path_maps = path_pre_pre
         # load the maps
         path_map_left = os.path.join(path_maps, \
-                                    'maps/reach_map_6D_aboveTable_left.pkl')
+                                    'maps/reach_map_6D_left_clipped.pkl')
         path_map_right = os.path.join(path_maps, \
-                                    'maps/reach_map_6D_aboveTable_right.pkl')
-        map_left = pickle.load(open(path_map_left,"rb"))    
-        map_right = pickle.load(open(path_map_right,"rb")) 
+                                    'maps/reach_map_6D_right_clipped.pkl')
+        map_left = pickle.load(open(path_map_left, "rb"))
+        map_right = pickle.load(open(path_map_right, "rb"))
 
-        end=timeit.default_timer()
-        print('Running time of load map: %s Seconds'%(end-start))
-        
+        end = timeit.default_timer()
+        print('Running time of load map: %s Seconds' % (end - start))
+
         return map_left, map_right
 
-    def score(self,map,pos):              
+    def score(self, map, pos):
         # reach map,poses -> list of scores
         list_score = []
         for p in pos:
             # find the nearst cell in the reach map
-            pose = pos.reshape(-1)
-            squ_dist = np.sum((map[:,:3]-pose[:3])**2,axis=1)
-            inds_min_dist = np.argwhere(squ_dist<=squ_dist[np.argmin(squ_dist)]).reshape(-1)
-            cell = map[inds_min_dist,:]
-            
+            pose = p.reshape(-1)
+            squ_dist = np.sum((map[:, :3] - pose[:3])**2, axis=1)
+            inds_min_dist = np.argwhere(
+                squ_dist <= squ_dist[np.argmin(squ_dist)]).reshape(-1)
+            cell = map[inds_min_dist, :]
+
             # in the cell find the similarst rotation
-            squ_rota = np.sum((cell[:,3:6]-pose[3:6])**2,axis=1)
-            inds_min_rota = np.argwhere(squ_rota<=squ_rota[np.argmin(squ_rota)]).reshape(-1)
-            
-            list_score.append(map[inds_min_dist[inds_min_rota],7])
-            
+            squ_rota = np.sum((cell[:, 3:6] - pose[3:6])**2, axis=1)
+            inds_min_rota = np.argwhere(
+                squ_rota <= squ_rota[np.argmin(squ_rota)]).reshape(-1)
+
+            list_score.append(map[inds_min_dist[inds_min_rota], 7])
+
+            x = np.argwhere(map[:,0]==round(pose[0],))
+            y = np.argwhere(map[:,1]==pose[1])
+            z = np.argwhere(map[:,2]==pose[2])
+            print(x,y,z)
+
+        print("x:",np.min(map[:,0]),"~",np.max(map[:,0]))
+        print("y:",np.min(map[:,1]),"~",np.max(map[:,1]))
+        print("z:",np.min(map[:,2]),"~",np.max(map[:,2]))
+        print("pos:",pos.reshape(-1)[0:3])
+        print("-----------------------------------------")
+
         return list_score
 
-
-
-    def selectArm(self,score_l, score_r): 
-        if score_l==0 and score_r==0:
+    def selectArm(self, score_l, score_r):
+        if score_l == 0 and score_r == 0:
             # print("the inputed pose isn't in the reach map")
-            return ""  
+            return ""
         elif score_l > score_r:
             # print("left arm")
             return "left"
         else:
             # print("right arm")
             return "right"
-        
+
     def getArm(self):
         # better reachability score -> select arm
-        map_left, map_right = self.reachMap() 
-        list_arm = []   
-  
-        start=timeit.default_timer()
-        
-        list_score_l = self.score(map_left, self.pos)  # minmal distance and index
+        map_left, map_right = self.reachMap()
+        list_arm = []
+
+        start = timeit.default_timer()
+
+        list_score_l = self.score(map_left,
+                                  self.pos)  # minmal distance and index
         list_score_r = self.score(map_right, self.pos)
-              
-        end=timeit.default_timer()
-        print('Running time of get scores: %s Seconds'%(end-start))
-        
+
+        end = timeit.default_timer()
+        print('Running time of get scores: %s Seconds' % (end - start))
+
         for i in range(len(list_score_l)):
-            list_arm.append(self.selectArm(list_score_l[i], list_score_r[i]))  # unreachable="", left="left", right="right"
-        print("score_l:",list_score_l)
-        print("score_r",list_score_r)
+            list_arm.append(self.selectArm(
+                list_score_l[i],
+                list_score_r[i]))  # unreachable="", left="left", right="right"
+        print("score_l:", list_score_l)
+        print("score_r", list_score_r)
         # print(arm)
-        if len(list_arm)==1:
+        if len(list_arm) == 1:
             return list_arm[0]
         else:
             return list_arm
 
 
-     
-    
 if __name__ == '__main__':
-    x = -1 + 2*random.random()
-    y = -1 + 2*np.random.random()
-    z = -1 + 2*random.random()
-    rx = (-1 + 2*random.random())*3
-    ry = (-1 + 2*np.random.random())*1.4
-    rz = (-1 + 2*random.random())*3
-    pose1 = np.array(([[x,y,z,rx,ry,rz]]))
-    pose2 = np.array(([[x,y,z,rx,ry,rz],[x,y,z,rx,ry,rz],[x,y,z,rx,ry,rz]]))
-    
-    print("shape of input poses:", pose2.shape)
-    arm = Arm(pose2)    
-    print(arm.getArm()) 
-  
+    x = -0.9 + 1.7 * (np.random.random())  # x: -0.92499995 ~ 0.875
+    y = -0.3 + 1.4 * (np.random.random())  # y: -0.325 ~ 1.125
+    z = 0.425 + 0.1 * (np.random.random())  # z: 0.425 ~ 0.52500004
+    rx = (-1 + 2 * random.random()) * 3
+    ry = (-1 + 2 * np.random.random()) * 1.4
+    rz = (-1 + 2 * random.random()) * 3
+    pose1 = np.array(([[x, y, z, rx, ry, rz]]))
+    pose2 = np.array(([[x, y, z, rx, ry, rz], [x, y, z, rx, ry, rz],
+                       [x, y, z, rx, ry, rz]]))
+
+    print("shape of input poses:", pose1.shape)
+    arm = Arm(pose1)
+    print(arm.getArm())
