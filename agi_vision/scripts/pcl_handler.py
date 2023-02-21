102b6ebfd1e4b845e90ab0c212dc2695bdce799b
RiicK3d <40563427+RiicK3d@users.noreply.github.com>
2023-02-21 08:58:47 +0000

    Color detection, find n_clusters, Perception-msg

--- agi_vision/scripts/pcl_handler.py
+++ agi_vision/scripts/pcl_handler.py
@@ -21,6 +21,11 @@ from shape_msgs.msg import Plane
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 from utils import k_means_clustering, XYZRGB_to_XYZ, find_edge, get_pose
 # https://answers.ros.org/question/59725/publishing-to-a-topic-via-subscriber-callback-function/
+
+
+from agi_vision.msg import PerceptionMSG
+
+
 class PCLHandler:
     def __init__(self):
         self.node = rospy.init_node('pcl_handling', anonymous=True)
@@ -29,6 +34,8 @@ class PCLHandler:
         self.pub_pcl_filter = rospy.Publisher('Pointcloud2_filtered', PointCloud2, queue_size=10)
         self.pub_pcl_planes = rospy.Publisher('Pointcloud2_cube_planes', PointCloud2, queue_size=10)
         self.pub_pcl_edges = rospy.Publisher('Edge_points', PointCloud2, queue_size=10)
+
+        self.pub_perception = rospy.Publisher('PerceptionMSG', PerceptionMSG, queue_size=10)
 
 
         self.pub1_edge = rospy.Publisher('Edge_point1', PointCloud2, queue_size=10)
@@ -57,7 +64,7 @@ class PCLHandler:
         # for p in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True):
         #     print " x : %f  y: %f  z: %f" %(p[0],p[1],p[2])
 
-        n_clusters = 3  # TODO make config!
+        n_clusters_fixed = 8 # TODO make config!
         
         
         pcl_data = self.ros_to_pcl(data)
@@ -90,9 +97,86 @@ class PCLHandler:
         indices, plane_coeff = self.segment_pcl(cloud_plane,pcl.SACMODEL_PLANE)
         cloud_plane = cloud_filtered.extract(indices, negative=False)     
 
-       
+        # find n_cluster!
+        find_n_cluster = True
+        n_clusters = n_clusters_fixed
+
+        if find_n_cluster:
+            current_n = 1
+            thr_max = 1500
+            thr_min = 500
+            running = True
+            while running:
+                print("number of clusters")
+                print(current_n)
+                old_n = current_n
+
+                # clustering
+                cluster_indices, centroids = k_means_clustering(np.array(cloud_plane), current_n, 1000)
+
+                for i in range(current_n):
+                    # clustered_points = cloud_plane.extract(cluster_indices[i], negative=False)
+
+                    # cluster it so small 
+                    if len(cluster_indices[i]) < thr_min:
+                        print("Cluster is very small!")
+
+                    # cluster is to big, split it up 
+                    elif len(cluster_indices[i]) > thr_max:
+                        print("Cluster is too big!")
+                        too_much = 0 
+                        current_n +=1
+                        break
+                        
+                    # cluster between min and max
+                    else:  # len(cluster_indices[i]) < thr_max:
+                    
+                        # point distance stuff
+                        # clustered_points = cloud_plane.extract(cluster_indices[i], negative=False)
+                        # best_point, indices = find_edge(clustered_points, centroids[i])
+                        # array_cloud=np.array(XYZRGB_to_XYZ(points))
+                        # center=centroids[i][0:3]
+                        # distances=np.linalg.norm(array_cloud-center,axis=1)
+
+                        # distcnace between centroids should be more than 5 cm
+                        # print(centroids)
+                        for j in range(i, len(centroids), 1):
+                            too_much = 0
+                            # print(j)
+                            # dont compare same cluster 
+                            if i != j:
+                                distance = np.linalg.norm(centroids[i][:3]-centroids[j][:3])
+
+                                if distance < 0.03:
+                                    too_much += 1
+                                    print("Clusters are at the same cube!")
+                                    print(distance)
+                                    print(i)
+                                    print(j)
+
+                if old_n == current_n:
+                    n_clusters = current_n - too_much
+                    running = False
+
+            print("final number of clusters")
+            print(n_clusters)
+
         # Do clustering 
         clustered_points=cloud_plane
+
+        if n_clusters == n_clusters_fixed:
+            print("found exact number of clusters!")
+        else:
+            print("find cluster amount failed!")
+            print("number of clusters found is")
+            print(n_clusters)
+            print("number of real clusters is")
+            print(n_clusters_fixed)
+            print("using n_clusters:")
+            # TODO: turn off to use found number of clusters
+            n_clusters = n_clusters_fixed
+            print(n_clusters)
+
         # white_cloud = self.XYZRGB_to_XYZ(cloud_filtered) 
         cluster_indices, centroids=k_means_clustering(np.array(cloud_plane),n_clusters,1000)
         # cluster_indices=DBSCAN_clustering(np.array(cloud_plane))
@@ -129,6 +213,7 @@ class PCLHandler:
         
         for i in range(n_clusters):
             clustered_points = cloud_plane.extract(cluster_indices[i], negative=False)
+            print(clustered_points)
             edge_points, best_point, indices = find_edge(clustered_points, centroids[i])
             #
             interest_point_list.append(edge_points)
@@ -142,6 +227,8 @@ class PCLHandler:
             r_all = 0
             g_all = 0
             b_all = 0
+            y_all = 0
+
             for data in clustered_array:
                 res = "not found"
                 s = struct.pack('>f', data[3])
@@ -151,29 +238,35 @@ class PCLHandler:
                 r = (pack & 0x00FF0000) >> 16
                 g = (pack & 0x0000FF00) >> 8
                 b = (pack & 0x000000FF)
-                color_min = min([r, g, b])
 
-                r_all += r - color_min
-                g_all += g - color_min
-                b_all += b - color_min
-
-            # decides color on maximum r value
-            # TODO: add threshold as redish planes are not real red!
-            if r_all > g_all:
-                if r_all > b_all:
-                    res = "red"
+                if r > (b+50) and g > (b+50):
+                    y_all +=1
                 else:
-                    res = "blue"
-            else:  # g_all > r_all
-                if g_all > b_all:
-                    res = "green"
-                else:
-                    res = "blue"
+                    if r > (g+50) and r > (b+50):
+                        r_all += 1
+                    if g > (r+30) and g > (b+10):
+                        g_all += 1
+                    if b > (r+25):
+                        b_all += 1
 
+            # print(r_all)
+            # print(g_all)
+            # print(b_all)
+            # print(y_all)
+
+            input_list = [r_all, b_all, g_all, y_all]
+            max_value = max(input_list)
+            index = input_list.index(max_value)
+            names = ["red", "blue", "green", "yellow"]
+            res = names[index]
+            numbers = np.array(input_list)
+            # Check if all values in array are zeros
+            if np.all(numbers == 0) and len(input_list) > 0:
+                res = "no_color_found"
             color_list.append(res)
-            # print(i)
 
             ########### end of loop
+
         print(color_list)
 
         cube_poses.header.frame_id = "base_footprint"
@@ -203,6 +296,14 @@ class PCLHandler:
         self.pub_pcl_planes.publish(pcl_seg_msg)  # cube planes 
         # self.pub2_pcl2.publish(pcl_clustered)  # edge points 
         self.pub3_pose.publish(cube_poses)  # cube poses 
+
+        perc_msg = PerceptionMSG()
+        perc_msg.pose_array = cube_poses
+        perc_msg.color_names = color_list
+        perc_msg.size = len(color_list)
+        self.pub_perception.publish(perc_msg)
+
+
         # self.pub2_pcl2.publish(pcl_seg_transf)
         # self.pub_pcl2.publish(pcl_seg_msg)
 
