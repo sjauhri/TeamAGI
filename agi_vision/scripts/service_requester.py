79df84c6fa68d429077c32104236ab278d1dd3d2
RiicK3d <40563427+RiicK3d@users.noreply.github.com>
2023-03-27 18:29:43 +0000

    Perception service

---
+++ agi_vision/scripts/service_requester.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# # Import modules
+# import rospy
+# # import ros_numpy
+# import pcl
+# # https://github.com/strawlab/python-pcl
+# import numpy as np
+# import ctypes
+# import struct
+# import sensor_msgs.point_cloud2 as pc2
+
+# from sensor_msgs.msg import PointCloud2, PointField
+# from std_msgs.msg import Header
+# from random import randint
+# from std_msgs.msg import String
+# from sensor_msgs.msg import Image 
+import rospy
+from pcl_handler import PCLHandler
+from pcl_service import PCLService
+import sys 
+from agi_vision.srv import PerceptionSRV
+from geometry_msgs.msg import PoseStamped
+
+
+if __name__ == '__main__':
+    rospy.wait_for_service('PerceptionService')
+    perc_srv = rospy.ServiceProxy('PerceptionService', PerceptionSRV)
+    try:
+        loc = PoseStamped()
+        perc_res = perc_srv(n_stacked=int(3))
+        print(perc_res)
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
